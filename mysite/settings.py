22d1930b9e43abf282729ba474c390bdab0e1f0a
jm0sp1923 <jm0sp1923@gmail.com>
2024-10-16 11:19:44 -0500

    crsf conf

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,6 +51,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://la-patrana-production.up.railway.app',
+]
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
